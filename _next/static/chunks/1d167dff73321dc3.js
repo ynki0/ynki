df0687c7670ca7d63b8fa922bd26927d51eba26c
YNKI <88197049+ynki0@users.noreply.github.com>
2025-12-10 11:31:19 +0300

    Delete _next directory

--- _next/static/chunks/1d167dff73321dc3.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/825faae467b6f85d.js",
-  "static/chunks/ca8487da2abc1171.js",
-  "static/chunks/21e422afb0d35cf8.js",
-  "static/chunks/ed66033fc0c26c04.css",
-  "static/chunks/turbopack-b306d4e02bec94e9.js"
-])
--- _next/static/chunks/8ba573ff4a2f5240.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/59dc01f05693598e.js",
-  "static/chunks/ca8487da2abc1171.js",
-  "static/chunks/turbopack-69e3e79284fd3e6a.js"
-])
