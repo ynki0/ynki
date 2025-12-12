0b24e4284715e9c12a303378b6e2a113d0fd81c8
YNKI <88197049+ynki0@users.noreply.github.com>
2025-12-12 22:43:03 +0300

    Add files via upload

---
+++ _next/static/chunks/c01a1a44d0ac3172.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/825faae467b6f85d.js",
+  "static/chunks/ca8487da2abc1171.js",
+  "static/chunks/55c50d14492741e0.js",
+  "static/chunks/0890191e726eb0a6.css",
+  "static/chunks/turbopack-a2a4a2c7d6e6d00d.js"
+])
