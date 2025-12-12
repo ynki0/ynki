da843a5d293cc01b91d7521202576c2967987837
YNKI <88197049+ynki0@users.noreply.github.com>
2025-12-12 21:30:33 +0300

    Add files via upload

---
+++ _next/static/chunks/443d491bafe22483.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/825faae467b6f85d.js",
+  "static/chunks/ca8487da2abc1171.js",
+  "static/chunks/55c50d14492741e0.js",
+  "static/chunks/b2375dc9a12077cc.css",
+  "static/chunks/turbopack-4475b450eae7b84b.js"
+])
---
+++ _next/static/lpXCL2UrdMcUCiU_0U1Ws/_buildManifest.js
@@ -0,0 +1,23 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/443d491bafe22483.js"
+  ],
+  "/_error": [
+    "static/chunks/8ba573ff4a2f5240.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/.//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
