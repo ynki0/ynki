52223b5c16628da8f26f78fc657afda770161535
YNKI <88197049+ynki0@users.noreply.github.com>
2025-12-25 01:29:36 +0300

    Add files via upload

---
+++ ru/_next/static/chunks/2c86d8bd5e67b529.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/web-development", [
+  "static/chunks/6954bb6a384e6e7e.js",
+  "static/chunks/7e869a5ff664249b.js",
+  "static/chunks/ea3e0875166e57be.js",
+  "static/chunks/b7a5791f47e3c794.js",
+  "static/chunks/bdca9cc96193e61c.css",
+  "static/chunks/turbopack-ec9158024b9ecca3.js"
+])
---
+++ ru/_next/static/chunks/cf9beeace55e8442.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/ui-ux-design", [
+  "static/chunks/661dc6bcf1ee8cec.js",
+  "static/chunks/7e869a5ff664249b.js",
+  "static/chunks/ea3e0875166e57be.js",
+  "static/chunks/b7a5791f47e3c794.js",
+  "static/chunks/bdca9cc96193e61c.css",
+  "static/chunks/turbopack-35676a8149768a6a.js"
+])
