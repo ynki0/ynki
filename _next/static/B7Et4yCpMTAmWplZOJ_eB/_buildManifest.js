ba3272b1975a5f0c01faf7e287074055e338e27e
YNKI <88197049+ynki0@users.noreply.github.com>
2025-12-25 13:29:09 +0300

    Add files via upload

---
+++ _next/static/B7Et4yCpMTAmWplZOJ_eB/_buildManifest.js
@@ -0,0 +1,87 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/87a0e37236aeee70.js"
+  ],
+  "/_error": [
+    "static/chunks/6ea96f2339555d4a.js"
+  ],
+  "/en": [
+    "static/chunks/befbe60ae09f623b.js"
+  ],
+  "/en/about": [
+    "static/chunks/a1e9aada9ff5eb54.js"
+  ],
+  "/en/app-development": [
+    "static/chunks/47e0c101a0fb7659.js"
+  ],
+  "/en/automation-ai-solutions": [
+    "static/chunks/8612f1820893ba16.js"
+  ],
+  "/en/pricing": [
+    "static/chunks/687b290b57a23525.js"
+  ],
+  "/en/software-development": [
+    "static/chunks/dec8ea6cda6afabd.js"
+  ],
+  "/en/ui-ux-design": [
+    "static/chunks/ea8518deb872e213.js"
+  ],
+  "/en/web-development": [
+    "static/chunks/ba2830d8bee1fb39.js"
+  ],
+  "/ru": [
+    "static/chunks/0102585869224227.js"
+  ],
+  "/ru/about": [
+    "static/chunks/b4dc426675b96c92.js"
+  ],
+  "/ru/app-development": [
+    "static/chunks/2ce1ce8f98c9a680.js"
+  ],
+  "/ru/automation-ai-solutions": [
+    "static/chunks/64b281443225160f.js"
+  ],
+  "/ru/pricing": [
+    "static/chunks/0c44258337277f01.js"
+  ],
+  "/ru/software-development": [
+    "static/chunks/4bbfca30b42065bf.js"
+  ],
+  "/ru/ui-ux-design": [
+    "static/chunks/c468ae649d63e5d5.js"
+  ],
+  "/ru/web-development": [
+    "static/chunks/6a8ce188f11f6590.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/.//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/en",
+    "/en/about",
+    "/en/app-development",
+    "/en/automation-ai-solutions",
+    "/en/pricing",
+    "/en/software-development",
+    "/en/ui-ux-design",
+    "/en/web-development",
+    "/ru",
+    "/ru/about",
+    "/ru/app-development",
+    "/ru/automation-ai-solutions",
+    "/ru/pricing",
+    "/ru/software-development",
+    "/ru/ui-ux-design",
+    "/ru/web-development"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/826c2a938f301cd8.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/ce0ddbb2d2b9ed23.js",
+  "static/chunks/b7a5791f47e3c794.js",
+  "static/chunks/ea3e0875166e57be.js",
+  "static/chunks/e7dc659de3e3f0b8.css",
+  "static/chunks/turbopack-cf767eea3a921306.js"
+])
